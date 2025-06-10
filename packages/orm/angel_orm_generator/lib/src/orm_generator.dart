f596b19b048a5215255a0d8be67c19da6857e72a
Philac35 <philacta35@gmail.com>
2025-06-10 19:00:54 +0200

    Modify [Wip] Orm_generator,  toJson() Class model 10/06/2025 19h00

--- packages/orm/angel_orm_generator/lib/src/orm_generator.dart
+++ packages/orm/angel_orm_generator/lib/src/orm_generator.dart
@@ -314,7 +314,7 @@ class Angel3OrmGenerator extends GeneratorForAnnotation<Orm> {
           ..name = 'toJson'
           ..annotations.add(refer('override'))
           ..returns = refer('Map<String, dynamic>')
-          ..body = Code('return ${replaceLastWord(modelClassName,"Model", "")}Serializer.toMap(this);');
+          ..body = Code('return ${modelClassName.replaceRange(modelClassName.lastIndexOf('Model'),modelClassName.length, "")}Serializer.toMap(this);');
 
 
 
@@ -341,12 +341,6 @@ class Angel3OrmGenerator extends GeneratorForAnnotation<Orm> {
     lib.body.add(classBuilder.build());
   }
 
-
-  String replaceLastWord(String source, String word, String replacement) {
-    // \b is a word boundary, $ is end of string, (?s) makes . match newlines
-    final regExp = RegExp(r'\b' + RegExp.escape(word) + r'\b(?!.*\b' + RegExp.escape(word) + r'\b)', dotAll: true);
-    return source.replaceFirst(regExp, replacement);
-  }
 
   void generateQueryMethods(ClassBuilder b, String className, ClassElement element,
       List<String> fieldNames, String tableNameStr) {
